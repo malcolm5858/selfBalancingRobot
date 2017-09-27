0485d1a62fdb65af431f9dab6e76fc556e964be8
Malcolm Machesky <malcolm9292@gmail.com>
2017-09-27 14:39:56 -0400

    started pid
    last commit for 9/27/2017

--- selfbalancingRobot/selfbalancingRobot.ino
+++ selfbalancingRobot/selfbalancingRobot.ino
@@ -1,3 +1,13 @@
+#include <PID_v1.h>
+
+
+double Setpoint, Input, Output;
+
+double Kp=2, Ki=5, Kd=1;
+PID gyroPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
