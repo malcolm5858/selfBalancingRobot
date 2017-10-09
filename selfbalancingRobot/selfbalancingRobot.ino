948e686507b6576fb783e5ee18fed741d0d860bf
malcolmmachesky <malcolm9292@gmail.com>
2017-10-09 14:35:16 -0400

    Code for monday
    added ace and played with PID

--- selfbalancingRobot/selfbalancingRobot.ino
+++ selfbalancingRobot/selfbalancingRobot.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+
 #include <MPU6050.h>
 
 #include <Servo.h>
@@ -8,17 +10,79 @@
 double Setpoint, Input, Output;
 
 double Kp=2, Ki=5, Kd=1;
-PID gyroPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-
+PID pid(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+MPU6050 mpu;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
+  checkSettings();
+
+  pid.setMode(AUTOMATIC);
+  pid.setSampleTime(10);
+  pid.setOutputLimits(-100, 100);
 }
+
+void checkSettings(){
+  Serial.println;
+  Serial.print(" * Sleep Mode:            ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Clock Source:          ");
+  switch (mpu.getClockSource) {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  Serial.print(" * Accelerometer:         ");
+  switch(mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }
+
+  Serial.print(" * Accelerometer offsets: ");
+  Serial.print(mpu.getAccelOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getAccelOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getAccelOffsetZ());
+
+  Serial.println();
+}
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    Vector rawAccel mpu.readRawAccel();
+    Vector normAccel mpu.readNormalizeAcell();
+
+    // Serial.print(" Xraw = ");
+    // Serial.print(rawAccel.XAxis);
+    // Serial.print(" Yraw = ");
+    // Serial.print(rawAccel.YAxis);
+    // Serial.print(" Zraw = ");
+    //
+    // Serial.println(rawAccel.ZAxis);
+    // Serial.print(" Xnorm = ");
+    // Serial.print(normAccel.XAxis);
+    // Serial.print(" Ynorm = ");
+    // Serial.print(normAccel.YAxis);
+    // Serial.print(" Znorm = ");
+    // Serial.println(normAccel.ZAxis);
+    //
+    // delay(10);
 
 }
-//test
